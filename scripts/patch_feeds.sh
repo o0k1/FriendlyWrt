5b5b70b0d1ad2f048ab7bd77721df9362b4c1011
root <root@localhost>
2026-08-03 22:21:46 +0800

    Update patch_feeds.sh

--- scripts/patch_feeds.sh
+++ scripts/patch_feeds.sh
@@ -6,11 +6,11 @@ if [ ! -z "$1" ]; then
 	OPENWRT_VERSION="$1"
 fi
 
-if [[ "${OPENWRT_VERSION}" =~ ^24\..+$ ]]; then
-  # Bump go version for xtls-core (always use latest)
-  rm -rf feeds/packages/lang/golang
-  git clone https://github.com/o0k1/openwrt-packages-lang-golang -b coolsnowwolf-master feeds/packages/lang/golang
-fi
+# if [[ "${OPENWRT_VERSION}" =~ ^24\..+$ ]]; then
+#   # Bump go version for xtls-core (always use latest)
+#   rm -rf feeds/packages/lang/golang
+#   git clone https://github.com/o0k1/openwrt-packages-lang-golang -b coolsnowwolf-master feeds/packages/lang/golang
+# fi
 
 # Set Rust build arg llvm.download-ci-llvm to false.
 # Thanks to https://github.com/Shuery-Shuai/ImmortalWrt-BPI-R4-Firmware/commit/17bffb5b345fb04be81a6a3632b5c341607172fc
